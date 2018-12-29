f45542535eeb739671db7cdf573296107e4477f9
Hod Greeley <hod@greeley.org>
2018-12-28 16:16:53 -0800

    Switch to androidx

--- android/app/src/main/java/com/couchbase/mobile/zebra/CatalogActivity.java
+++ android/app/src/main/java/com/couchbase/mobile/zebra/CatalogActivity.java
@@ -1,13 +1,12 @@
 package com.couchbase.mobile.zebra;
 
-import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 
 import com.couchbase.lite.Dictionary;
 
--- android/app/src/main/java/com/couchbase/mobile/zebra/ScanActivity.java
+++ android/app/src/main/java/com/couchbase/mobile/zebra/ScanActivity.java
@@ -12,9 +12,9 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
